6eccd8bc3fa5b3b249d6f750a8598ef126d05a26
tensho1026 <tensho1026@icloud.com>
2025-01-06 07:49:17 +0900

    xclone作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import LeftBar from "@/components/LeftBar";
 import "./globals.css";
+import RightBar from "@/components/RightBar";
 
 export default function RootLayout({
   children,
@@ -6,8 +8,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between'>
+          <div className='px-2 xsm:px-4 xxl:px-8'>
+            <LeftBar />
+          </div>
+          <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray'>{children}</div>
+          <div className='hidden lg:flex ml-4 md:ml-8 flex-1'>
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+// "use client";
+// import Image from "next/image";
+// import { IKImage } from "imagekitio-next";
+// const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
-export default Homepage
+
+// const Homepage = () => {
+//   return (
+//     <div className=''>
+//       <IKImage
+//         urlEndpoint={urlEndpoint}
+//         path='general/post.jpeg'
+//         width={600}
+//         height={600}
+//         alt='test post'
+//       />
+//     </div>
+//   );
+// };
+
+// export default Homepage;
+"use client";
+import { IKImage } from "imagekitio-next";
+
+const Homepage = () => {
+  const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT || "";
+
+  if (!urlEndpoint) {
+    return <p>URL Endpoint is not defined</p>;
+  }
+
+  return (
+    <div>
+      <IKImage
+        urlEndpoint={urlEndpoint}
+        path="general/post.jpeg"
+        width={600}
+        height={600}
+        alt="test post"
+      />
+    </div>
+  );
+};
+
+export default Homepage;
